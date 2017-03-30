94e93d302ac3a27f4da46ed16fb46c1f88ce735b
William Reynolds <billy.reynolds@gmail.com>
2017-03-30 01:15:45 -0500

    Added an about page and url pattern

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hey there partner! Welcome to Rango.')
+    return HttpResponse('Hey there partner! Welcome to Rango. <a href="about/">About</a>')
+
+
+def about(request):
+    return HttpResponse('Rango says here is the about page. <a href="/rango/">Go back</a>')
