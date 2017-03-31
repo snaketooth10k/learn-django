d6e2a488d76d2c48f4bfa72934e55e0afb30e429
William Reynolds <billy.reynolds@gmail.com>
2017-03-30 23:33:19 -0500

    Refactored to reduce repetition Added a link on index

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -3,7 +3,7 @@ from rango.models import Category, Page
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -14,8 +14,8 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter a page title.")
-    url = forms.URLField(max_length=200, help_text="Please enter the URL for the page.")
+    title = forms.CharField(help_text="Please enter a page title.")
+    url = forms.URLField(help_text="Please enter the URL for the page.")
     views = forms.CharField(widget=forms.HiddenInput, initial=0)
 
     class Meta:
