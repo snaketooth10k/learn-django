f5a944c78a1d39ed6d377e1eb9906400fef31e66
Will <williamareynolds@icloud.com>
2017-03-31 16:05:59 -0500

    Corrected some validation issues and template URLs

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -6,7 +6,7 @@ app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^add-category/$', views.add_category, name='add_category'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_slug>[\w\-]+)/$', views.show_category, name='show_category'),
     url(r'^category/(?P<category_slug>[\w\-]+)/add-page/$', views.add_page, name='add_page'),
 ]
