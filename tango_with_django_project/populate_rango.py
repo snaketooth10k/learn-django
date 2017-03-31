524376cc00d8cfc9d74970b840b8b64321187691
William Reynolds <billy.reynolds@gmail.com>
2017-03-30 19:38:12 -0500

    Populate views for pages

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -14,26 +14,34 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
+         'views': 24,
          'url': "http://docs.python.org/2/tutorial/"},
         {"title": "How to Think like a Computer Scientist",
+         'views': 15,
          "url": "http://www.greenteapress.com/thinkpython/"},
         {"title": "Learn Python in 10 Minutes",
+         'views': 78,
          "url": "http://www.korokithakis.net/tutorials/python/"}
     ]
 
     django_pages = [
         {"title": "Official Django Tutorial",
+         'views': 13,
          "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
         {"title": "Django Rocks",
+         'views': 22,
          "url": "http://www.djangorocks.com/"},
         {"title": "How to Tango with Django",
+         'views': 77,
          "url": "http://www.tangowithdjango.com/"}
     ]
 
     other_pages = [
         {"title": "Bottle",
+         'views': 14,
          "url": "http://bottlepy.org/docs/dev/"},
         {"title": "Flask",
+         'views': 22,
          "url": "http://flask.pocoo.org"}
     ]
 
@@ -45,7 +53,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category = c):
