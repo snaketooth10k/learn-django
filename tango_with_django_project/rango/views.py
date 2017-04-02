6b6b049fc8fe2eed3d1df30bf63ee49a38c07062
William Reynolds <billy.reynolds@gmail.com>
2017-04-02 14:48:09 -0500

    Created register view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,7 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category, Page
-from rango.forms import CategoryForm, PageForm
+from rango.forms import *
 
 # Create your views here.
 
@@ -57,6 +57,43 @@ def index(request):
         'pages': page_list
     }
     return render(request, 'rango/index.xhtml', context=context_dict)
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    context_dict = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'registered': registered,
+    }
+
+    return render(request, 'rango/register.xhtml', context_dict)
 
 
 def show_category(request, category_slug):
