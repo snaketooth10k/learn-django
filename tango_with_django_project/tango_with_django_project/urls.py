b15a95619b220a6d72a656ec2aaa3b710632982e
Snaketooth10k <billy.reynolds@gmail.com>
2017-04-04 17:10:31 -0500

    added url path for login module

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -22,6 +22,7 @@ from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^rango/', include('rango.urls')),
+    url(r'^account/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^rango/', include('rango.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
