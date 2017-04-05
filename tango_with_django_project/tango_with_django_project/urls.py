a584cc28b61121839b61cc372a4cd6f9d81ccc51
Snaketooth10k <billy.reynolds@gmail.com>
2017-04-05 16:03:15 -0500

    Updated url mapping with override

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -19,9 +19,17 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from registration.backends.simple.views import RegistrationView
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^rango/', include('rango.urls')),
